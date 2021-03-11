24abdd612773c6562d9b5e97740d375c6e6f9374
jpjanic <jasmin.pjanic@yahoo.com>
2021-03-12 00:58:35 +0100

    Added index file

---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+
+    //Only change code below this line
+
+    var myFirstName = "Jasmin";
+    var myLastName = "Pjanic";
+    var myHomeTown = "Tuzla";
+    
+    //Only change code above this line
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
